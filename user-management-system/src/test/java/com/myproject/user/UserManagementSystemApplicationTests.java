3f724a1503a744450903ca23dda9ccc26a77f3a0
makaranddhamale <makarand.dhamale09@gmail.com>
2023-09-17 20:49:49 +0530

    User-management-system initial commit

---
+++ user-management-system/src/test/java/com/myproject/user/UserManagementSystemApplicationTests.java
@@ -0,0 +1,11 @@
+package com.myproject.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
