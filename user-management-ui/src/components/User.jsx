b487a14be4fbe4e94ca2121594f3dbc7373aa339
makaranddhamale <makarand.dhamale09@gmail.com>
2023-09-19 22:48:18 +0530

    Edit button implementation completed

--- user-management-ui/src/components/User.jsx
+++ user-management-ui/src/components/User.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const User = ({ user }) => {
+const User = ({ user, deleteUser, editUser }) => {
   return (
     <tr key={user.id}>
       <td className="text-center px-6 py-4 whitespace-nowrap">
@@ -18,12 +18,14 @@ const User = ({ user }) => {
       <td className="text-center px-6 py-4 whitespace-nowrap">
         <a
           href="#"
+          onClick={(e, id) => editUser(e, user.id)}
           className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer px-4"
         >
           Edit
         </a>
         <a
           href="#"
+          onClick={(e, id) => deleteUser(e, user.id)}
           className="text-indigo-600 hover:text-indigo-800 hover:cursor-pointer"
         >
           Delete
--- user-management-ui/src/components/UserList.jsx
+++ user-management-ui/src/components/UserList.jsx
@@ -1,17 +1,19 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import User from "./User";
+import EditUser from "./EditUser";
 
 const UserList = ({ user }) => {
-  const USER_API_BASE_URL = "http://localhost:8080/api/v1/user/allUsers";
+  const USER_API_BASE_URL = "http://localhost:8080/api/v1/user";
   const [users, setUsers] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [userId, setUserId] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await fetch(USER_API_BASE_URL, {
+        const response = await fetch(USER_API_BASE_URL + "/allUsers", {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
@@ -27,39 +29,65 @@ const UserList = ({ user }) => {
     fetchData();
   }, [user]);
 
+  const deleteUser = (e, id) => {
+    e.preventDefault();
+    fetch(USER_API_BASE_URL + "/" + id, {
+      method: "DELETE",
+    }).then((res) => {
+      if (users) {
+        setUsers((prevElement) => {
+          return prevElement.filter((user) => user.id !== id);
+        });
+      }
+    });
+  };
+
+  const editUser = (e, id) => {
+    e.preventDefault();
+    setUserId(id);
+  };
+
   return (
-    <div className="container mx-auto my-8">
-      <div className="flex shadow border-b">
-        <table className="min-w-full">
-          <thead className="bg-gray-50">
-            <tr>
-              <th className="text-center font-medium text-gray-500 uppercase tracking-wide py-3">
-                Sr No.
-              </th>
-              <th className="text-center font-medium text-gray-500 uppercase tracking-wide py-3">
-                First Name
-              </th>
-              <th className="text-center font-medium text-gray-500 uppercase tracking-wide py-3">
-                Last Name
-              </th>
-              <th className="text-center font-medium text-gray-500 uppercase tracking-wide py-3">
-                Email Id
-              </th>
-              <th className="text-center font-medium text-gray-500 uppercase tracking-wide py-3">
-                Actions
-              </th>
-            </tr>
-          </thead>
-          {!loading && (
-            <tbody>
-              {users?.map((user) => (
-                <User user={user} key={user.id} />
-              ))}
-            </tbody>
-          )}
-        </table>
+    <>
+      <div className="container mx-auto my-8">
+        <div className="flex shadow border-b">
+          <table className="min-w-full">
+            <thead className="bg-gray-50">
+              <tr>
+                <th className="text-center font-medium text-gray-500 uppercase tracking-wide py-3">
+                  Sr No.
+                </th>
+                <th className="text-center font-medium text-gray-500 uppercase tracking-wide py-3">
+                  First Name
+                </th>
+                <th className="text-center font-medium text-gray-500 uppercase tracking-wide py-3">
+                  Last Name
+                </th>
+                <th className="text-center font-medium text-gray-500 uppercase tracking-wide py-3">
+                  Email Id
+                </th>
+                <th className="text-center font-medium text-gray-500 uppercase tracking-wide py-3">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            {!loading && (
+              <tbody>
+                {users?.map((user) => (
+                  <User
+                    user={user}
+                    key={user.id}
+                    deleteUser={deleteUser}
+                    editUser={editUser}
+                  />
+                ))}
+              </tbody>
+            )}
+          </table>
+        </div>
       </div>
-    </div>
+      <EditUser userId={userId} setUserId={setUserId} />
+    </>
   );
 };
 
